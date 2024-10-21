660f16b94dbe2491f6e0a9b303a62c6303e3e69d
Rohmat Sidiq Mustaqim <rohmatsidiq9@gmail.com>
2024-10-21 10:24:26 +0700

    membuat pixel

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import { ConfigProvider } from "antd";
+import Script from "next/script";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,7 +28,6 @@ export default function RootLayout({ children }) {
         <ConfigProvider
           theme={{
             token: {
-              // Seed Token
               colorPrimary: "#ffd800",
               borderRadius: 30,
             },
@@ -35,6 +35,35 @@ export default function RootLayout({ children }) {
         >
           {children}
         </ConfigProvider>
+
+        {/* Facebook Pixel Script */}
+        <Script
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window, document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1202037347751704');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1202037347751704&ev=PageView&noscript=1"
+            alt="facebook pixel"
+          />
+        </noscript>
       </body>
     </html>
   );
