b0b0bd86dedc24e97b31135df28900ee6ff338ba
Rohmat Sidiq Mustaqim <rohmatsidiq9@gmail.com>
2024-10-21 11:43:39 +0700

    buat tag manager

--- app/layout.js
+++ app/layout.js
@@ -1,70 +1,108 @@
-import localFont from "next/font/local";
-import "./globals.css";
-import { ConfigProvider } from "antd";
-import Script from "next/script";
+// pages/_document.js
+import { Html, Head, Main, NextScript } from "next/document";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
-export const metadata = {
-  title: "Dieza Digital - Jasa Pembuatan Website",
-  description: "Jasa Pembuatan Website dan Aplikasi Web",
-};
-
-export default function RootLayout({ children }) {
+export default function Document() {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-yellow-50`}
-      >
-        <ConfigProvider
-          theme={{
-            token: {
-              colorPrimary: "#ffd800",
-              borderRadius: 30,
-            },
-          }}
-        >
-          {children}
-        </ConfigProvider>
-
-        {/* Facebook Pixel Script */}
-        <Script
-          strategy="afterInteractive"
+    <Html lang="en">
+      <Head>
+        {/* Google Tag Manager */}
+        <script
           dangerouslySetInnerHTML={{
             __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '1202037347751704');
-              fbq('track', 'PageView');
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MGHSTQCQ');
             `,
           }}
         />
-
+        {/* End Google Tag Manager */}
+      </Head>
+      <body>
+        {/* Google Tag Manager (noscript) */}
         <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1202037347751704&ev=PageView&noscript=1"
-            alt="facebook pixel"
-          />
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MGHSTQCQ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
         </noscript>
+        <Main />
+        <NextScript />
       </body>
-    </html>
+    </Html>
   );
 }
+
+// import localFont from "next/font/local";
+// import "./globals.css";
+// import { ConfigProvider } from "antd";
+// import Script from "next/script";
+
+// const geistSans = localFont({
+//   src: "./fonts/GeistVF.woff",
+//   variable: "--font-geist-sans",
+//   weight: "100 900",
+// });
+// const geistMono = localFont({
+//   src: "./fonts/GeistMonoVF.woff",
+//   variable: "--font-geist-mono",
+//   weight: "100 900",
+// });
+
+// export const metadata = {
+//   title: "Dieza Digital - Jasa Pembuatan Website",
+//   description: "Jasa Pembuatan Website dan Aplikasi Web",
+// };
+
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <body
+//         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-yellow-50`}
+//       >
+//         <ConfigProvider
+//           theme={{
+//             token: {
+//               colorPrimary: "#ffd800",
+//               borderRadius: 30,
+//             },
+//           }}
+//         >
+//           {children}
+//         </ConfigProvider>
+
+//         {/* Facebook Pixel Script */}
+//         <Script
+//           strategy="afterInteractive"
+//           dangerouslySetInnerHTML={{
+//             __html: `
+//               !function(f,b,e,v,n,t,s)
+//               {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+//               n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+//               if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+//               n.queue=[];t=b.createElement(e);t.async=!0;
+//               t.src=v;s=b.getElementsByTagName(e)[0];
+//               s.parentNode.insertBefore(t,s)}(window, document,'script',
+//               'https://connect.facebook.net/en_US/fbevents.js');
+//               fbq('init', '1202037347751704');
+//               fbq('track', 'PageView');
+//             `,
+//           }}
+//         />
+
+//         <noscript>
+//           <img
+//             height="1"
+//             width="1"
+//             style={{ display: "none" }}
+//             src="https://www.facebook.com/tr?id=1202037347751704&ev=PageView&noscript=1"
+//             alt="facebook pixel"
+//           />
+//         </noscript>
+//       </body>
+//     </html>
+//   );
+// }
